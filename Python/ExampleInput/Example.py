e471fdc419921dc02ed63443910b2eb79b5fe9d5
Paul <paul.ockenfuss@physik.uni-muenchen.de>
2019-05-14 14:46:53 +0200

    Example for Input logger
    Minimal Example on how to use the Input logger. Read in Exemplary data file and produce another data file as output.

---
+++ Python/ExampleInput/Example.py
@@ -0,0 +1,22 @@
+from Python import Input as Inp
+import argparse
+import numpy as np
+
+#########################################################################
+#An Example program on how to use the input logger
+#########################################################################
+VERSION="1.0"
+par=argparse.ArgumentParser()
+par.add_argument('infile')
+par.add_argument('-s',action='store_true')
+args=par.parse_args()
+print(args.infile)
+inp=Inp.Input(args.infile,version=VERSION)
+inp.convert_type(int, "option2")
+inp.show_data()
+
+dat=np.genfromtxt(inp.get("data"))
+
+if args.s:
+    inp.write_log(inp.get("save"), inp.get("data"), file_ext="log")
+    np.savetxt(inp.get("save"),dat**2)
