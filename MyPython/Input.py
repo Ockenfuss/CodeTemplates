8ab3e6805b05ff80be6e7bb3e4324c9bea1bb750
Paul_Meteo <paul.ockenfuss@physik.uni-muenchen.de>
2020-01-09 16:58:35 +0100

    Input Logger support default sections Possibility to specify sections for default values

--- MyPython/Input.py
+++ MyPython/Input.py
@@ -4,7 +4,7 @@ import configparser
 import os
 import __main__
 import datetime
-VERSION="1.3.1"
+VERSION="1.4.0"
 
 
 
@@ -37,23 +37,33 @@ class Input(object):
         self.filename=infilename
         self.version=version
         self.options={}
-        self.config = configparser.ConfigParser(def_opts)
+        self.config = configparser.ConfigParser()
         self.config._interpolation = configparser.ExtendedInterpolation()
+        for sec in def_opts:
+            self.options[sec]={}
+            for key in def_opts[sec]:
+                self.options[sec][key]=def_opts[sec][key]
         if infilename is not None:
             self.config.read(infilename)
         for sec in self.config:
-            self.options[sec]={}
+            if not (sec in self.options):
+                self.options[sec]={}
             for key in self.config[sec]:
                 self.options[sec][key]=self.config[sec][key]
 
-    def getKeys(self, option, section):
+    def getKey(self, option, section):
         """Check the existence of the given keys and choose defaults if they are 'None'."""
         if section==None:
             section="DEFAULT"#possibility to specify standard section
         if option==None:
             option=list(self.options[section].keys())[0]#possibility to specify standard option
+        option=option.lower()
         return option, section
 
+    def listKeys(self, section):
+        if section==None:
+            section=="DEFAULT"
+        return self.options[section].keys()
     def get(self,  option=None,section=None):
         """Return the specified option.
 
@@ -64,7 +74,7 @@ class Input(object):
         Returns:
             value -- value for the given option in the given section 
         """
-        option, section=self.getKeys(option, section)
+        option, section=self.getKey(option, section)
         return self.options[section][option]
 
     def set(self, value, option=None, section=None):
@@ -77,7 +87,7 @@ class Input(object):
             option {str} -- Option to be set. (default: {None})
             section {str} -- Section where the option is located. (default: {None})
         """
-        option, section=self.getKeys(option, section)
+        option, section=self.getKey(option, section)
         self.options[section][option]=value
 
     def convert_type(self, dtype, option=None, section=None):
@@ -90,8 +100,8 @@ class Input(object):
             option {string} -- The option to be converted. (default: {None})
             section {string} -- The section where the option is located (default: {None})
         """
-        option, section=self.getKeys(option, section)
-        myDict={int: self.config.getint, float: self.config.getfloat, bool: self.config.getboolean}
+        option, section=self.getKey(option, section)
+        myDict={int: lambda sec, opt: int(self.get(opt, sec)), float: lambda sec, opt: float(self.get(opt, sec)), bool: lambda sec, opt: self.get(opt, sec).lower() in ("true", "yes", "1", "t")}
         self.set(myDict.get(dtype)(section, option),option=option, section=section)
 
     def convert_array(self, dtype, option=None, section=None, sep=",", removeSpaces=False):
@@ -106,13 +116,13 @@ class Input(object):
             sep {string} -- The separator between the array values (default: {","})
             removeSpaces {bool} -- Remove spaces in the elements when converting to string array. (default: {False})
         """
-        option, section=self.getKeys(option, section)
+        option, section=self.getKey(option, section)
         if dtype==str:
-            array=self.config[section][option].split(sep)
+            array=self.options[section][option].split(sep)
             if removeSpaces:
                 array=[x.strip() for x in array]
         else:
-            array=np.fromstring(self.config[section][option], sep=sep, dtype=dtype)
+            array=np.fromstring(self.options[section][option], sep=sep, dtype=dtype)
         self.set(array, option, section)
 
     def create_log(self):
